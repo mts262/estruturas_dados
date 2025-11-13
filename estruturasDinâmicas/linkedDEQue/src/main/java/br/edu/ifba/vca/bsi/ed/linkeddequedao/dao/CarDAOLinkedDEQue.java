b67af1afa302c0527765a15bab32e76041714ab5
Matheus Pereira Rodrigues <mts027@gmail.com>
2025-11-13 15:27:17 -0300

    Finaliza implementação de CarDAOLinkedDEQue

--- estruturasDinâmicas/linkedDEQue/src/main/java/br/edu/ifba/vca/bsi/ed/linkeddequedao/dao/CarDAOLinkedDEQue.java
+++ estruturasDinâmicas/linkedDEQue/src/main/java/br/edu/ifba/vca/bsi/ed/linkeddequedao/dao/CarDAOLinkedDEQue.java
@@ -76,13 +76,13 @@ public class CarDAOLinkedDEQue implements CarDAO{
 
     /**
      * Atualiza um carro da fila
-     * 
+     *
      * @param newCar, carro a ser atualizado
      */
     @Override
     public void updateCar(Car newCar) {
         DEQueable<Car> tempCars = new LinkedDEQue<>(20);
-        
+
         while (!cars.isEmpty()) {
             Car car = cars.beginDequeue();
             if (car.getLicensePlate().equalsIgnoreCase(newCar.getLicensePlate())) {
@@ -98,7 +98,7 @@ public class CarDAOLinkedDEQue implements CarDAO{
 
     /**
      * Remove um carro da fila pela sua placa
-     * 
+     *
      * @param plateLicense, placa do carro a ser removido
      * @return carro removido
      */
@@ -204,7 +204,7 @@ public class CarDAOLinkedDEQue implements CarDAO{
     public Car[] getCarsByMark(String mark) {
         DEQueable<Car> tempCars = new LinkedDEQue<>(20);
         DEQueable<Car> foundCars = new LinkedDEQue<>(20);
-        
+
         while (!cars.isEmpty()) {
             Car car = cars.beginDequeue();
             tempCars.endEnqueue(car);
@@ -212,7 +212,7 @@ public class CarDAOLinkedDEQue implements CarDAO{
                 foundCars.endEnqueue(car);
             }
         }
-        
+
         while (!tempCars.isEmpty()) {
             cars.endEnqueue(tempCars.beginDequeue());
         }
@@ -323,13 +323,13 @@ public class CarDAOLinkedDEQue implements CarDAO{
         while (!tempCars.isEmpty()) {
             cars.endEnqueue(tempCars.beginDequeue());
         }
-        
+
         return dequeToArray(foundCars);
     }
 
     /**
      * Busca os carro que estão estacionados há mais de um determinado número de horas
-     * 
+     *
      * @param thresholdHours, número de horas de permanênci mínima
      * @return array com os carros encontrados
      */
@@ -353,7 +353,7 @@ public class CarDAOLinkedDEQue implements CarDAO{
         while (!tempCars.isEmpty()) {
             cars.endEnqueue(tempCars.beginDequeue());
         }
-        
+
         return dequeToArray(foundCars);
     }
 
@@ -399,7 +399,7 @@ public class CarDAOLinkedDEQue implements CarDAO{
     public Car getCarByNewestArrival() {
         DEQueable<Car> tempCars = new LinkedDEQue<>(20);
         Car resultCar = null;
-        
+
         while (!cars.isEmpty()) {
             Car car = cars.beginDequeue();
             tempCars.endEnqueue(car);
@@ -418,7 +418,7 @@ public class CarDAOLinkedDEQue implements CarDAO{
                 }
             }
         }
-        
+
         while (!tempCars.isEmpty()) {
             cars.endEnqueue(tempCars.beginDequeue());
         }
@@ -471,46 +471,237 @@ public class CarDAOLinkedDEQue implements CarDAO{
         return resultCar;
     }
 
+    /**
+     * Imprime todos os carros que estão na fila
+     *
+     * @return String representando os carros da fila
+     */
     @Override
     public String printCars() {
         return cars.toString();
     }
 
+    /**
+     * Retorna a quantidade de carros presentes na fila
+     *
+     * @return int com o número total
+     */
     @Override
     public int getTotalCars() {
         return countElements(cars);
     }
 
+    /**
+     * Retorna a marca mais popular da fila
+     *
+     * @return marca mais popular
+     */
     @Override
     public String getMostPopularMark() {
-        return "";
+        DEQueable<Car> tempCars = new LinkedDEQue<>(20);
+        String resultMark = null;
+        int counted = 0;
+
+        if (!cars.isEmpty()) {
+            Car car = cars.beginDequeue();
+            tempCars.endEnqueue(car);
+            resultMark = car.getMark();
+            counted = countMarkOcurrences(cars, car.getMark());
+        }
+
+        while (!cars.isEmpty()) {
+            Car car = cars.beginDequeue();
+            tempCars.endEnqueue(car);
+            int countMark = countMarkOcurrences(cars, car.getMark());
+            if (countMark > counted) {
+                resultMark = car.getMark();
+                counted = countMark;
+            }
+        }
+
+        while (!tempCars.isEmpty()) {
+            cars.endEnqueue(tempCars.beginDequeue());
+        }
+
+        return resultMark;
     }
 
+    /**
+     * Retorna o modelo mais popular da fila
+     *
+     * @return modelo mais popular
+     */
     @Override
     public String getMostPopularModel() {
-        return "";
+        DEQueable<Car> tempCars = new LinkedDEQue<>(20);
+        String resultModel = null;
+        int counted = 0;
+
+        if (!cars.isEmpty()) {
+            Car car = cars.beginDequeue();
+            tempCars.endEnqueue(car);
+            resultModel = car.getModel();
+            counted = countModelOcurrences(cars, car.getModel());
+
+        }
+
+        while (!cars.isEmpty()) {
+            Car car = cars.beginDequeue();
+            tempCars.endEnqueue(car);
+            int countModel = countModelOcurrences(cars, car.getModel());
+            if (countModel > counted) {
+                resultModel = car.getModel();
+                counted = countModel;
+            }
+        }
+
+        while (!tempCars.isEmpty()) {
+            cars.endEnqueue(tempCars.beginDequeue());
+        }
+
+        return resultModel;
     }
 
+    /**
+     * Retorna a cor mais popular da fila
+     *
+     * @return cor mais popular
+     * @throws java.util.NoSuchElementException, se não houver carros com cor definida
+     */
     @Override
     public String getMostPopularColor() {
-        return "";
+        DEQueable<Car> tempCars = new LinkedDEQue<>(20);
+        String resultColor = null;
+        int counted = 0;
+
+        while (!cars.isEmpty()) {
+            Car car = cars.beginDequeue();
+            tempCars.endEnqueue(car);
+            if (car.getColor() != null) {
+                resultColor = car.getColor();
+                counted = countColorOcurrences(cars, car.getColor());
+                break;
+            }
+
+        }
+
+        while (!cars.isEmpty()) {
+            Car car = cars.beginDequeue();
+            tempCars.endEnqueue(car);
+            if (car.getColor() != null) {
+                int countColor = countColorOcurrences(cars, car.getColor());
+                if (countColor > counted) {
+                    resultColor = car.getColor();
+                    counted = countColor;
+                }
+            }
+        }
+
+        while (!tempCars.isEmpty()) {
+            cars.endEnqueue(tempCars.beginDequeue());
+        }
+
+        if (resultColor == null) {
+            throw new NoSuchElementException("Não há carros com cor definida");
+        }
+
+        return resultColor;
     }
 
+    /**
+     * Retorna o tempo que um carro está estacionado em minutos
+     *
+     * @param plateLicense placa do carro a ser verificado o tempo que está estacionado
+     * @return tempo estacionado em minutos
+     * @throws java.util.NoSuchElementException, se o carro não for encontrado
+     *         ou se não tiver a data de chegada definida
+     */
     @Override
     public long getParkingDuration(String plateLicense) {
-        return 0;
+        DEQueable<Car> tempCars = new LinkedDEQue<>(20);
+        LocalDateTime now = LocalDateTime.now();
+        Long parkedMinutes = null;
+
+        while (!cars.isEmpty()) {
+            Car car = cars.beginDequeue();
+            tempCars.endEnqueue(car);
+            if (car.getLicensePlate().equalsIgnoreCase(plateLicense) && car.getArrived() != null) {
+                parkedMinutes = Duration.between(car.getArrived(), now).toMinutes();
+                break;
+            }
+        }
+
+        while (!tempCars.isEmpty()) {
+            cars.beginEnqueue(tempCars.endDequeue());
+        }
+
+        if (parkedMinutes == null) {
+            throw new NoSuchElementException("Carro não encontrado ou com data de chegada não definida");
+        }
+
+        return parkedMinutes;
     }
 
+    /**
+     * Retorna todos os carros que tem a sua duração estacionada dentro de um intervalo de tempo delimitado
+     *
+     * @param minHours hora mínima do intevalo
+     * @param maxHours hora máxima do intervalo
+     * @return array com carros encontrados
+     */
     @Override
     public Car[] getCarsByParkingDuration(long minHours, long maxHours) {
-        return new Car[0];
+        DEQueable<Car> tempCars = new LinkedDEQue<>(20);
+        DEQueable<Car> foundCars = new LinkedDEQue<>(20);
+        LocalDateTime now = LocalDateTime.now();
+
+        while (!cars.isEmpty()) {
+            Car car = cars.beginDequeue();
+            tempCars.endEnqueue(car);
+            if (car.getArrived() != null) {
+                long duration = Duration.between(car.getArrived(), now).toHours();
+                if (duration >= minHours && duration <= maxHours) {
+                    foundCars.endEnqueue(car);
+                }
+            }
+        }
+
+        while (!tempCars.isEmpty()) {
+            cars.endEnqueue(tempCars.beginDequeue());
+        }
+
+        return dequeToArray(foundCars);
     }
 
+    /**
+     * Verifica se um carro está no estacionamento
+     *
+     * @param plateLicense placa do carro a ser verificado
+     * @return true se o carro estiver no estacionamento, false caso contrário
+     */
     @Override
     public boolean isCarInPlaced(String plateLicense) {
-        return false;
+        DEQueable<Car> tempCars = new LinkedDEQue<>(20);
+        boolean placed = false;
+
+        while (!cars.isEmpty()) {
+            Car car = cars.beginDequeue();
+            tempCars.endEnqueue(car);
+            if (car.getLicensePlate().equalsIgnoreCase(plateLicense)) {
+                placed = true;
+            }
+        }
+
+        while (!tempCars.isEmpty()) {
+            cars.endEnqueue(tempCars.beginDequeue());
+        }
+
+        return placed;
     }
 
+    /**
+     * Restaura a fila de carros, removendo todos os seus elementos
+     */
     @Override
     public void clearAllCars() {
         while (!cars.isEmpty()) {
@@ -518,29 +709,82 @@ public class CarDAOLinkedDEQue implements CarDAO{
         }
     }
 
+    /**
+     * Calcula a quantidade de espaços disponíveis
+     *
+     * @return o número de vagas
+     */
     @Override
     public int getAvailableSpaces() {
-        return 0;
+        return getMaxCapacity() - countElements(cars);
     }
 
+    /**
+     * Retorna a taxa de ocupação do estacionamento em porcentagem
+     *
+     * @return int respresentando a porcentagem de vagas ocupadas
+     */
     @Override
     public int getOccupancyRate() {
-        return 0;
+        int totalCars = countElements(cars);
+        int maxCapacity = getMaxCapacity();
+
+        if (maxCapacity == 0) {
+            return 0;
+        }
+
+        return (totalCars * 100) / maxCapacity;
     }
 
+    /**
+     * Verifica se a fila de carros está cheia
+     *
+     * @return true se estiver cheia, false caso contrário
+     */
     @Override
     public boolean isParkingFull() {
-        return false;
+        return cars.isFull();
     }
 
+    /**
+     * Verifica se a fila de carros está vazia
+     *
+     * @return true se estiver vazia, false caso contrário
+     */
     @Override
     public boolean isParkingEmpty() {
-        return false;
+        return cars.isEmpty();
     }
 
+    /**
+     * Conta a capacidade máxima da fila de carros
+     *
+     * @return capacidade máxima
+     */
     @Override
     public int getMaxCapacity() {
-        return 0;
+        DEQueable<Car> tempCars = new LinkedDEQue<>(countElements(cars));
+        int maxCapacity = 0;
+
+        while (!cars.isEmpty()) {
+            tempCars.endEnqueue(cars.beginDequeue());
+        }
+
+        while (!cars.isFull()) {
+            cars.endEnqueue(new Car("XXX0000", "xx", "xx"));
+            maxCapacity++;
+        }
+
+
+        while (!cars.isEmpty()) {
+            cars.beginDequeue();
+        }
+
+        while (!tempCars.isEmpty()) {
+            cars.endEnqueue(tempCars.beginDequeue());
+        }
+
+        return maxCapacity;
     }
 
     /**
@@ -576,6 +820,84 @@ public class CarDAOLinkedDEQue implements CarDAO{
 
         while (!tempCars.isEmpty()) {
             deque.endEnqueue(tempCars.beginDequeue());
+        }
+
+        return count;
+    }
+
+    /**
+     * Conta o número de vezes que uma marca aparece na fila
+     *
+     * @param aux, fila auxiliar a ser percorrida
+     * @param mark, marca a ser contada na fila
+     * @return o número de vezes que a marca apareceu
+     */
+    private int countMarkOcurrences(DEQueable<Car> aux, String mark) {
+        DEQueable<Car> tempAux = new LinkedDEQue<>(20);
+        int count = 0;
+
+        while (!aux.isEmpty()) {
+            Car car = aux.beginDequeue();
+            tempAux.endEnqueue(car);
+            if (car.getMark().equalsIgnoreCase(mark)) {
+                count++;
+            }
+        }
+
+        while (!tempAux.isEmpty()) {
+            aux.endEnqueue(tempAux.beginDequeue());
+        }
+
+        return count;
+    }
+
+    /**
+     * Conta o número de vezes que um modelo aparece na fila
+     *
+     * @param aux, fila auxiliar a ser percorrida
+     * @param model, modelo a ser contado na fila
+     * @return o número de vezes que o modelo apareceu
+     */
+    private int countModelOcurrences(DEQueable<Car> aux, String model) {
+        DEQueable<Car> tempAux = new LinkedDEQue<>(20);
+        int count = 0;
+
+        while (!aux.isEmpty()) {
+            Car car = aux.beginDequeue();
+            tempAux.endEnqueue(car);
+            if (car.getModel() != null && car.getModel().equalsIgnoreCase(model)) {
+                count++;
+            }
+        }
+
+        while (!tempAux.isEmpty()) {
+            aux.endEnqueue(tempAux.beginDequeue());
+        }
+
+        return count;
+    }
+
+    /**
+     * Conta o número de vezes que uma cor aparece na fila
+     *
+     * @param aux, fila auxiliar a ser percorrida
+     * @param color, cor a ser contada na fila
+     * @return o número de vezes que a cor apareceu
+     */
+    private int countColorOcurrences(DEQueable<Car> aux, String color) {
+        DEQueable<Car> tempAux = new LinkedDEQue<>(20);
+        int count = 0;
+
+        while (!aux.isEmpty()) {
+            Car car = aux.beginDequeue();
+            tempAux.endEnqueue(car);
+            if (car.getColor() != null && car.getColor().equalsIgnoreCase(color)) {
+                count++;
+            }
+        }
+
+        while (!tempAux.isEmpty()) {
+            aux.endEnqueue(tempAux.beginDequeue());
         }
 
         return count;
